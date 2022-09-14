367007daf10835ff18fb91d919ba901bf93e7f4d
exceloids <lhorexcelbombarda@gmail.com>
2022-09-14 17:16:33 +0800

    M307 Upload

--- Push.bat
+++ Push.bat
@@ -1,5 +1,7 @@
 @echo off
 git remote set-url origin https://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@github.com/exceloids/Bombarda_CIT14.git
+git config --global user.name "exceloids"
+git config --global user.email lhorexcelbombarda@gmail.com
 pause
 git add .
 git commit -m "M307 Upload"
