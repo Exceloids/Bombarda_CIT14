d394b80fa568e00895b3c863350c8b0945e41992
CIT142F-45@M307.UC-BCF.EDU.PH <CIT142F-45@M307.UC-BCF.EDU.PH>
2022-09-14 17:10:06 +0800

    M307 Upload

--- Push.bat
+++ Push.bat
@@ -1,4 +1,6 @@
 @echo off
+git remote set-url origin https://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@github.com/exceloids/Bombarda_CIT14.git
+pause
 git add .
 git commit -m "M307 Upload"
 git push
